d25b39dbdab38f29634f419ff609ff1cc197ebe1
Tushar Singh <tusharsingh@192.168.0.105>
2023-03-31 15:27:21 +0530

    add female

--- customerServer.js
+++ customerServer.js
@@ -22,11 +22,11 @@ let  customersData  = [
   {id: "KPW09", name:"Pradeep", city:"Gurgaon", age:21, gender:"Male", payment:"Wallet"},
   {id: "ABR12", name:"Rohit", city:"Jaipur", age:34, gender:"Male", payment:"Debit Card"},
   {id: "BR451", name:"Preeti", city:"Delhi", age:29, gender:"Female", payment:"Credit Card"},
-  {id: "MKR52", name:"Neha", city:"Noida", age:42, gender:" Female ", payment:"Debit Card"},
-  {id: "BTT66", name:"Swati", city:"Gurgaon", age:24, gender:" Female ", payment:"Wallet"},
-  {id: "CDP09", name:"Meghna", city:"Jaipur", age:38, gender:" Female ", payment:"Debit Card"},
+  {id: "MKR52", name:"Neha", city:"Noida", age:42, gender:"Female", payment:"Debit Card"},
+  {id: "BTT66", name:"Swati", city:"Gurgaon", age:24, gender:"Female", payment:"Wallet"},
+  {id: "CDP09", name:"Meghna", city:"Jaipur", age:38, gender:"Female", payment:"Debit Card"},
   {id: "KK562", name:"Irfan", city:"Delhi", age:25, gender:"Male", payment:"Credit Card"},
-  {id: "LPR34", name:"Gagan", city:"Noida", age:51, gender:" Female ", payment:"Debit Card"},
+  {id: "LPR34", name:"Gagan", city:"Noida", age:51, gender:"Female", payment:"Debit Card"},
   {id: "MQC11", name:"John", city:"Gurgaon", age:24, gender:"Male", payment:"Wallet"},
   {id: "AXY22", name:"Gurmeet", city:"Jaipur", age:31, gender:"Male", payment:"Debit Card"}
  ];
@@ -60,7 +60,7 @@ app.get("/customers", function (req, res) {
     arr1.sort((st1, st2) => (st1.age)-(st2.age));
   } 
 
-  res.send(arr1);  
+  res.send(arr1);   
 });
 
 app.get("/customers/:id",function (req, res) {
